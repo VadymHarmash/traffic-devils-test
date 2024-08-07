defa56cf6ee0ca9e0006b4537790b8f01550274e
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-08-07 23:25:30 +0300

    added validation

--- src/components/Main/Form/index.jsx
+++ src/components/Main/Form/index.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import intlTelInput from 'intl-tel-input'
 import 'intl-tel-input/build/css/intlTelInput.css'
 import styles from './form.module.scss'
 import placeholder from '../../../assets/images/main/form/placeholder.png'
 
 export default function Form() {
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [formErrors, setFormErrors] = useState({})
     const phoneInputRef = useRef(null)
 
     useEffect(() => {
@@ -21,12 +26,43 @@ export default function Form() {
             phoneInputElement.setAttribute('placeholder', `+${iti.getSelectedCountryData().dialCode}`)
 
             const handleCountryChange = () => phoneInputElement.setAttribute('placeholder', `+${iti.getSelectedCountryData().dialCode}`)
-
             phoneInputElement.addEventListener('countrychange', handleCountryChange)
 
-            return () => phoneInputElement.removeEventListener('countrychange', handleCountryChange)
+            return () => {
+                phoneInputElement.removeEventListener('countrychange', handleCountryChange)
+                if (iti) {
+                    iti.destroy()
+                }
+            }
         }
     }, [])
+
+    const validateForm = (e) => {
+        e.preventDefault()
+        const errors = {}
+
+        if (!name) errors.name = 'This field is required.'
+
+        if (!surname) errors.surname = 'This field is required.'
+
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+        if (!email) {
+            errors.email = 'This field is required.'
+        } else if (!emailPattern.test(email)) {
+            errors.email = 'Invalid email format.'
+        }
+
+        const phoneInputElement = phoneInputRef.current
+        const iti = intlTelInput(phoneInputElement)
+        const phoneNumber = iti.getNumber()
+        if (!phoneNumber) {
+            errors.phone = 'This field is required.'
+        } else if (!iti.isValidNumber()) {
+            errors.phone = 'Invalid phone number.'
+        }
+
+        setFormErrors(errors)
+    }
 
     return (
         <div className={styles.form}>
@@ -34,19 +70,48 @@ export default function Form() {
                 <div className={styles.form__wrapper}>
                     <p className={styles.form__text}>Investire 250 euro per raggiungere 5000 euro in una settimana vi sembra impossibile? Con il Gruppo Generali potete fare ancora di più!</p>
                     <div className={styles.form__polygon}><img src={placeholder} alt="News" /></div>
-                    <form className={styles.form__window}>
+                    <div className={styles.form__window}>
                         <div className={styles.form__window__inputs}>
-                            <input type="text" placeholder='il suo nome' />
-                            <input type="text" placeholder='il suo cognome' />
-                            <input type="email" placeholder='e-mail' />
-                            <input
-                                type="tel"
-                                ref={phoneInputRef}
-                                placeholder='+'
-                            />
+                            <div className={styles.form__window__inputWrapper}>
+                                <input
+                                    type="text"
+                                    placeholder='il suo nome'
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                                {formErrors.name && <span className={styles.error}>{formErrors.name}</span>}
+                            </div>
+                            <div className={styles.form__window__inputWrapper}>
+                                <input 
+                                    type="text" 
+                                    placeholder='il suo cognome'
+                                    value={surname}
+                                    onChange={(e) => setSurname(e.target.value)}
+                                />
+                                {formErrors.surname && <span className={styles.error}>{formErrors.surname}</span>}
+                            </div>
+                            <div className={styles.form__window__inputWrapper}>
+                                <input
+                                    type="text"
+                                    placeholder='e-mail'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                {formErrors.email && <span className={styles.error}>{formErrors.email}</span>}
+                            </div>
+                            <div className={styles.form__window__inputWrapper}>
+                                <input
+                                    type="tel"
+                                    ref={phoneInputRef}
+                                    value={phone}
+                                    placeholder='+'
+                                    onChange={(e) => setPhone(e.target.value)}
+                                />
+                                {formErrors.phone && <span className={styles.error}>{formErrors.phone}</span>}
+                            </div>
                         </div>
-                        <button>registrati</button>
-                    </form>
+                        <button onClick={validateForm}>registrati</button>
+                    </div>
                     <div className={styles.form__newsText}>
                         <h2>IL NOSTRO SCOPO</h2>
                         <p>Siamo qui per promuovere lo sviluppo e la circolazione delle persone, delle imprese e del Paese.</p>
